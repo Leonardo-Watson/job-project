47e4f6c7c105d213f11d2e4f30a61471c4e57cc3
Leonardo Watson <leonardo.watson.26@gmail.com>
2025-07-28 21:53:17 -0300

    refactor: add env into mongodb connection and remove products routes

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/product.model');
+const productRoute = require('./routes/product.route');
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use('/api/products', productRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello from Node API testing nodemon');
 });
 
-app.get('/api/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get('/api/product/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post('/api/products', async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 mongoose
   .connect(
-    'mongodb+srv://leonardowatson26:AHWX29pyvUToBfNQ@backenddb.pmwkpa4.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB'
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@backenddb.pmwkpa4.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB`
   )
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(3000, '0.0.0.0', () => {
+    app.listen(3000, () => {
       console.log(`Example app listening on port 3000`);
     });
   })
